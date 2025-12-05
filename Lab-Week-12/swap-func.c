2a056220adfe2752cea923bb5e2f655606118e38
Owais Raza <owaisrazasamad@gmail.com>
2025-12-05 20:51:10 +0500

    FOP Lab W12 (update)

--- Lab-Week-12/swap-func.c
+++ Lab-Week-12/swap-func.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int swapValues(int a, int b) {
+void swapValues(int a, int b)
+{
+    printf("Before swap, num1: %d, num2: %d \n", a, b);
+    int temp = a;
     a = b;
-    return a;
+    b = temp;
+    printf("After swap, num1: %d, num2: %d \n", a, b);
 }
 
-int main() {
-    int num1, num2, temp;
+int main()
+{
+    int num1, num2;
 
     printf("Enter value for first number: ");
     scanf("%d", &num1);
     printf("Enter value for second number: ");
     scanf("%d", &num2);
 
-    temp = num1;
-    num1 = swapValues(num1, num2);
-    num2 = temp;
-
-    printf("First number: %d \n", num1);
-    printf("Second number: %d", num2);
+    swapValues(num1, num2);
 }
